7ea9b043f8d2238fc39c46a20f85e2abdda321fd
Zhang Yifei <yiphyzhang@126.com>
2018-01-11 09:58:12 +0800

    Update main.cpp
    modify new test functions

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include<cv.h>
 #include<highgui.h>
 #include<iostream>
-
+#include<io.h>
 #include"ransac_line2d.h"
 #include"ransac_circle2d.h"
 #include"ransac_ellipse2d.h"
@@ -104,7 +104,7 @@ bool testLine2dMulti()
 		ep.x = ep.x - 10 * dx;
 		ep.y = ep.y - 10 * dy;
 
-		line(showMat, sp, ep, Scalar(0, 0, 255), 2, 8);
+		line(showMat, sp, ep, Scalar(0, 255, 0), 2, 8);
 
 		imshow("multiLines", showMat);
 		waitKey(1);
@@ -117,9 +117,10 @@ bool testLine2dMulti()
 		line2D.removeInliders(pCloud2D, inliers);
 	}
 
+	waitKey(-1);
+
 	cout << endl;
-
-
+	
 	return true;
 }
 
@@ -196,7 +197,7 @@ bool testCircle2dMulti()
 	sac::ModelCoefficient parameter;
 	circle2D.setDistanceThreshold(5);
 	circle2D.setMaxIterations(2500);
-	circle2D.setSpecificRadius(specRadius, 0.2);
+	//circle2D.setSpecificRadius(specRadius, 0.2);
 
 	while (pCloud2D.size() > 500)
 	{
@@ -210,10 +211,10 @@ bool testCircle2dMulti()
 
 		Point cp((int)parameter.modelParam[0], (int)parameter.modelParam[1]);
 		int radius = (int)parameter.modelParam[2];
-		circle(showMat, cp, radius, Scalar(0, 0, 255), 2, 8);
+		circle(showMat, cp, radius, Scalar(0, 255, 0), 2, 8);
 
 		imshow("circles", showMat);
-		waitKey(5);
+		waitKey(500);
 
 		cout << "Parameter of 2D line: < " << parameter.modelParam[0] << ", " <<
 			parameter.modelParam[1] << " >---" << parameter.modelParam[2] << endl;
@@ -221,59 +222,9 @@ bool testCircle2dMulti()
 		circle2D.removeInliders(pCloud2D, inliers);
 	}
 
+	waitKey();
+
 	cout << endl;
-
-	return true;
-}
-
-bool testEllipse2d()
-{
-	cv::Mat src = cv::imread("ellipsesTest.bmp", CV_LOAD_IMAGE_GRAYSCALE);
-
-	if (src.empty())
-		return false;
-
-	Mat inImg, showMat;
-	cvtColor(src, showMat, CV_GRAY2BGR);
-	threshold(src, inImg, 10, 255, CV_THRESH_BINARY_INV);
-
-	std::vector<sac::Point2D> pCloud2D;
-	for (int i = 0; i < inImg.rows; i++)
-	{
-		uchar* p = inImg.ptr<uchar>(i);
-		for (int j = 0; j < inImg.cols; j++)
-		{
-			if (p[j] != 0)
-				pCloud2D.push_back(sac::Point2D(j, i));
-		}
-	}
-
-	sac::ransacModelEllipse2D ellipse2D;
-	std::vector<int> inliers;
-	sac::ModelCoefficient parameter;
-	ellipse2D.setInputCloud(pCloud2D);
-	ellipse2D.setDistanceThreshold(5);
-	ellipse2D.setMaxIterations(25000);
-	ellipse2D.setSpecficAxisLength(135, 85, 0.2);
-	ellipse2D.computeModel();
-	ellipse2D.getInliers(inliers);
-	ellipse2D.getModelCoefficients(parameter);
-
-	cv::Point2f ellipseCenter; 
-	ellipseCenter.x = (float)parameter.modelParam[0];
-	ellipseCenter.y = (float)parameter.modelParam[1];
-
-	cv::Size2f ellipseSize; 
-	ellipseSize.width = (float)parameter.modelParam[2] * 2; 
-	ellipseSize.height = (float)parameter.modelParam[3] * 2;
-
-	float ellipseAngle = (float)parameter.modelParam[4];
-	cout << "Parameters of ellipse2D: < " << parameter.modelParam[0] << ", " <<
-		parameter.modelParam[1] << " > --- ";
-	cout << "Long/Short Axis: " << parameter.modelParam[2] << "/" << parameter.modelParam[3] << " --- ";
-	cout << "Angle: " << parameter.modelParam[4] << endl;
-
-	cv::ellipse(showMat, cv::RotatedRect(ellipseCenter, ellipseSize, ellipseAngle), cv::Scalar(0, 0, 255), 1, 8);
 
 	return true;
 }
@@ -305,10 +256,12 @@ bool testEllipse2dMulti()
 	sac::ModelCoefficient parameter;
 	ellipse2D.setDistanceThreshold(5);
 	ellipse2D.setMaxIterations(2500);
-	ellipse2D.setSpecficAxisLength(135, 85, 0.2);
+	ellipse2D.setSpecficAxisLength(120, 80, 0.3);
+	//ellipse2D.setSpecficAxisLength(135, 85, 0.5);
 
 	while (pCloud2D.size() > 500)
 	{
+		cout << pCloud2D.size() << endl;
 		ellipse2D.setInputCloud(pCloud2D);
 		ellipse2D.computeModel();
 		ellipse2D.getInliers(inliers);
@@ -317,21 +270,23 @@ bool testEllipse2dMulti()
 		if (inliers.size() < 500)
 			break;
 
-		//cv::Point2f ellipseCenter;
-		//ellipseCenter.x = (float)parameter.modelParam[0];
-		//ellipseCenter.y = (float)parameter.modelParam[1];
-		//cv::Size2f ellipseSize;
-		//ellipseSize.width = (float)parameter.modelParam[2] * 2;
-		//ellipseSize.height = (float)parameter.modelParam[3] * 2;
+		cv::Point2f ellipseCenter;
+		ellipseCenter.x = (float)parameter.modelParam[0];
+		ellipseCenter.y = (float)parameter.modelParam[1];
+		cv::Size2f ellipseSize;
+		ellipseSize.width = (float)parameter.modelParam[2] * 2;
+		ellipseSize.height = (float)parameter.modelParam[3] * 2;
 
-		//float ellipseAngle = (float)parameter.modelParam[4];
-		//cout << "Parameters of ellipse2D: < " << parameter.modelParam[0] << ", " <<
-		//	parameter.modelParam[1] << " > --- ";
-		//cout << "Long/Short Axis: " << parameter.modelParam[2] << "/" << parameter.modelParam[3] << " --- ";
-		//cout << "Angle: " << parameter.modelParam[4] << endl;
-		//cv::ellipse(showMat, cv::RotatedRect(ellipseCenter, ellipseSize, ellipseAngle), cv::Scalar(0, 0, 255), 3, 8);
-		//imshow("ellipses", showMat);
-		//waitKey(12);
+		float ellipseAngle = (float)parameter.modelParam[4];
+		cout << "Parameters of ellipse2D: < " << parameter.modelParam[0] << ", " <<
+			parameter.modelParam[1] << " > --- ";
+		cout << "Long/Short Axis: " << parameter.modelParam[2] << "/" << parameter.modelParam[3] << " --- ";
+		cout << "Angle: " << parameter.modelParam[4] << endl;
+		
+		cv::ellipse(showMat, cv::RotatedRect(ellipseCenter, ellipseSize, ellipseAngle), cv::Scalar(0, 255, 0), 2, 8);
+		
+		imshow("ellipses", showMat);
+		waitKey(12);
 
 		ellipse2D.removeInliders(pCloud2D, inliers);
 	}
@@ -339,12 +294,120 @@ bool testEllipse2dMulti()
 	return true;
 }
 
+bool testCircle2dPlus()
+{
+	cv::Mat src = cv::imread("circlesTest.bmp", CV_LOAD_IMAGE_GRAYSCALE);
+
+	if (src.empty())
+		return false;
+
+	Mat inImg, showMat;
+	cvtColor(src, showMat, CV_GRAY2BGR);
+	threshold(src, inImg, 10, 255, CV_THRESH_BINARY_INV);
+
+	std::vector<sac::Point2D> pCloud2D;
+
+	for (int i = 0; i < inImg.rows; i++)
+	{
+		uchar* p = inImg.ptr<uchar>(i);
+		for (int j = 0; j < inImg.cols; j++)
+		{
+			if (p[j] != 0)
+				pCloud2D.push_back(sac::Point2D(j, i));
+		}
+	}
+
+	vector<double> multiRaius; multiRaius.push_back(10); multiRaius.push_back(75);
+
+	sac::ransacModelCircle2D circle2D;
+	std::vector<int> inliers;
+	sac::ModelCoefficient parameter;
+	circle2D.setInputCloud(pCloud2D);
+	circle2D.setDistanceThreshold(5);
+	circle2D.setMaxIterations(2500);
+	circle2D.setMultiRadius(multiRaius, 0.2);
+	circle2D.computeModel();
+	circle2D.getInliers(inliers);
+	circle2D.getModelCoefficients(parameter);
+
+	Point cp((int)parameter.modelParam[0], (int)parameter.modelParam[1]);
+	int radius = (int)parameter.modelParam[2];
+	circle(showMat, cp, radius, Scalar(0, 0, 255), 2, 8);
+
+	cout << "Parameter of 2D circle: center < " << parameter.modelParam[0] << ", " <<
+		parameter.modelParam[1] << " > --- radius: " << parameter.modelParam[2] << endl;
+
+	return true;
+}
+
+bool testCircle2dMultiPlus()
+{
+	cv::Mat src = cv::imread("circlesTest.bmp", CV_LOAD_IMAGE_GRAYSCALE);
+
+	if (src.empty())
+		return false;
+
+	Mat inImg, showMat;
+	cvtColor(src, showMat, CV_GRAY2BGR);
+	threshold(src, inImg, 10, 255, CV_THRESH_BINARY_INV);
+
+	std::vector<sac::Point2D> pCloud2D;
+
+	for (int i = 0; i < inImg.rows; i++)
+	{
+		uchar* p = inImg.ptr<uchar>(i);
+		for (int j = 0; j < inImg.cols; j++)
+		{
+			if (p[j] != 0)
+				pCloud2D.push_back(sac::Point2D(j, i));
+		}
+	}
+
+	double specRadius = 100;
+
+	vector<double> multiRaius; multiRaius.push_back(100); multiRaius.push_back(75);
+	sac::ransacModelCircle2D circle2D;
+	std::vector<int> inliers;
+	sac::ModelCoefficient parameter;
+	circle2D.setDistanceThreshold(5);
+	circle2D.setMaxIterations(2000);
+	//circle2D.setSpecificRadius(specRadius, 0.2);
+	circle2D.setMultiRadius(multiRaius, 0.2);
+
+	while (pCloud2D.size() > 500)
+	{
+		circle2D.setInputCloud(pCloud2D);
+		circle2D.computeModel();
+		circle2D.getInliers(inliers);
+		circle2D.getModelCoefficients(parameter);
+
+		if (inliers.size() < parameter.modelParam[2] * 2 * CV_PI)
+			break;
+
+		//Point cp((int)parameter.modelParam[0], (int)parameter.modelParam[1]);
+		//int radius = (int)parameter.modelParam[2];
+		//circle(showMat, cp, radius, Scalar(0, 0, 255), 2, 8);
+
+		//imshow("circles", showMat);
+		//waitKey(5);
+
+		//cout << "Parameter of 2D line: < " << parameter.modelParam[0] << ", " << parameter.modelParam[1] << " >---" << parameter.modelParam[2] << endl;
+
+		circle2D.removeInliders(pCloud2D, inliers);
+	}
+
+	//cout << endl;
+
+	return true;
+}
+
 int main()
 {
-	while (true)
-	{
-		testEllipse2dMulti();
-	}
+	testEllipse2dMulti();
 	
+	testCircle2dMulti();
+	
+	testLine2dMulti();
+
 	return 1;
 }
